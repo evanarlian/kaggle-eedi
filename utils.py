e9df5f49925691a931b93a6ef04819cfc82ac54f
Evan Arlian <evanarlian2000@gmail.com>
2024-10-19 16:13:53 +0700

    temp weirdness on utils.py

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ def merge_with_misconceptions(
 
 def make_valid_df(df: pd.DataFrame) -> pd.DataFrame:
     """Creates a valid train or test dataframe. Valid means the misconception id must be not null
-    and the dataset is melted to ease row-by-row inference. We are need to melt 2 times
+    and the dataset is melted to ease row-by-row inference. For train, we are need to melt 2 times
     because we want to melt both answer and miconceptions in unison.
     """
     # 1. melt answers
@@ -37,10 +37,11 @@ def make_valid_df(df: pd.DataFrame) -> pd.DataFrame:
             "AnswerCText",
             "AnswerDText",
         ],
-        var_name="_melted_ans_header",  # rename the column that holds melted-column's headers
-        value_name="_melted_ans",  # rename the column that holds melted-column's content
+        var_name="WrongChoice",  # rename the column that holds melted-column's headers
+        value_name="Answer",  # rename the column that holds melted-column's content
     )
-    df_melted_ans = df_melted_ans.sort_values(["QuestionId", "_melted_ans_header"])
+    df_melted_ans["WrongChoice"] = df_melted_ans["WrongChoice"].str[6]
+    df_melted_ans = df_melted_ans.sort_values(["QuestionId", "WrongChoice"])
     try:
         # 2. melt misconceptions
         df_melted_mis = pd.melt(
@@ -53,9 +54,10 @@ def make_valid_df(df: pd.DataFrame) -> pd.DataFrame:
                 "MisconceptionDId",
             ],
             var_name="_melted_mis_header",
-            value_name="_melted_mis_id",
+            value_name="MisconceptionId",
         )
         df_melted_mis = df_melted_mis.sort_values(["QuestionId", "_melted_mis_header"])
+        df_melted_mis = df_melted_mis.drop(columns=["_melted_mis_header"])
         # 3. combine and cleam
         df_melted_mis = df_melted_mis.drop(columns="QuestionId")
         df_valid = pd.concat([df_melted_ans, df_melted_mis], axis=1)
@@ -65,4 +67,7 @@ def make_valid_df(df: pd.DataFrame) -> pd.DataFrame:
         # test set does not have misconceptions
         df_valid = df_melted_ans
     df_valid = df_valid.reset_index(drop=True)
+    df_valid["QuestionId_Answer"] = (
+        df_valid["QuestionId"].astype(str) + "_" + df_valid["WrongChoice"]
+    )
     return df_valid
