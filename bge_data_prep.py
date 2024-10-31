2b086a8a8076baf1facfecac58d1a261d67ed425
Evan Arlian <evanarlian2000@gmail.com>
2024-10-31 22:32:10 +0700

    finetune bge script

---
+++ bge_data_prep.py
@@ -0,0 +1,100 @@
+import json
+import time
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+
+import pandas as pd
+from sklearn.model_selection import GroupShuffleSplit
+
+from utils import make_valid_df
+
+
+@dataclass
+class Args:
+    dataset_dir: Path
+    output_dir: Path
+
+
+def make_jsonl(df: pd.DataFrame) -> list[dict]:
+    flag_emb_dataset = []
+    for i, row in df.iterrows():
+        subject = row["SubjectName"]
+        construct = row["ConstructName"]
+        question = row["QuestionText"]
+        mis = [f"MISCONCEPTION: {mis}" for mis in row["MisconceptionName"]]
+        anchor = f"QUESTION: {subject}. {construct}. {question}"
+        flag_row = {"query": anchor, "pos": mis}
+        flag_emb_dataset.append(flag_row)
+    return flag_emb_dataset
+
+
+def save_flag_emb_dataset(jsonl: list[dict], savepath: Path) -> None:
+    with open(savepath, "w") as f:
+        for row in jsonl:
+            f.write(json.dumps(row))
+            f.write("\n")
+
+
+def main(args: Args):
+    # prepare dataset
+    df_mis = pd.read_csv(args.dataset_dir / "misconception_mapping.csv")
+    df_full = pd.read_csv(args.dataset_dir / "train.csv")
+    df_full = make_valid_df(df_full)
+    df_full = pd.merge(
+        left=df_full,
+        right=df_mis,
+        left_on="MisconceptionIdLabel",
+        right_on="MisconceptionId",
+    )
+
+    # split dataset to train and val
+    splitter = GroupShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    train_idx, val_idx = next(splitter.split(df_full, groups=df_full["SubjectName"]))
+    df_train = df_full.iloc[train_idx]
+    df_val = df_full.iloc[val_idx]
+
+    # save the complete csv for evaluation later
+    bge_folder = args.output_dir / "bge_dataset"
+    bge_folder.mkdir(parents=True, exist_ok=True)
+    df_full.to_csv(bge_folder / "full.csv", index=False)
+    df_train.to_csv(bge_folder / "train.csv", index=False)
+    df_val.to_csv(bge_folder / "val.csv", index=False)
+
+    df_full_g = df_full.groupby(
+        ["SubjectName", "ConstructName", "QuestionText"], as_index=False
+    )[["MisconceptionName"]].agg(set)
+    df_train_g = df_train.groupby(
+        ["SubjectName", "ConstructName", "QuestionText"], as_index=False
+    )[["MisconceptionName"]].agg(set)
+    df_val_g = df_val.groupby(
+        ["SubjectName", "ConstructName", "QuestionText"], as_index=False
+    )[["MisconceptionName"]].agg(set)
+
+    # make 3 version of flag embedding dataset
+    jsonl_full = make_jsonl(df_full_g)
+    save_flag_emb_dataset(jsonl_full, bge_folder / "full.jsonl")
+    jsonl_train = make_jsonl(df_train_g)
+    save_flag_emb_dataset(jsonl_train, bge_folder / "train.jsonl")
+    jsonl_val = make_jsonl(df_val_g)
+    save_flag_emb_dataset(jsonl_val, bge_folder / "val.jsonl")
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--dataset-dir", required=True, type=Path, help="The csv folder"
+    )
+    parser.add_argument(
+        "--output-dir",
+        required=True,
+        type=Path,
+        help="Cleaned BGE dataset for training and validation.",
+    )
+    args = parser.parse_args()
+    args = Args(dataset_dir=args.dataset_dir, output_dir=args.output_dir)
+    print(args)
+    t0 = time.perf_counter()
+    main(args)
+    elapsed = time.perf_counter() - t0
+    print(f"done in {elapsed:.2f} secs.")
---
+++ scripts/finetune_bge.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+python bge_data_prep.py --dataset-dir=data --output-dir=data
+
+python -m FlagEmbedding.baai_general_embedding.finetune.hn_mine \
+    --model_name_or_path BAAI/bge-large-en-v1.5 \
+    --input_file data/bge_dataset/train.jsonl \
+    --output_file data/bge_dataset/train_mined_hn_stage1.jsonl \
+    --range_for_sampling 1-25 \
+    --negative_number 15 \
+    --use_gpu_for_searching 
+
+torchrun --nproc_per_node 1 \
+    -m FlagEmbedding.baai_general_embedding.finetune.run \
+    --output_dir models/bge_large_en_ft_stage1 \
+    --model_name_or_path BAAI/bge-large-en-v1.5 \
+    --train_data data/bge_dataset/train_mined_hn_stage1.jsonl \
+    --learning_rate 1e-5 \
+    --fp16 \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 16 \
+    --dataloader_drop_last True \
+    --normlized True \
+    --temperature 0.02 \
+    --query_max_len 150 \
+    --passage_max_len 35 \
+    --train_group_size 2 \
+    --negatives_cross_device \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --query_instruction_for_retrieval ""
+
+# TODO add validation here (make sure the format is MISCONCEPTION ... and )
+
+python -m FlagEmbedding.baai_general_embedding.finetune.hn_mine \
+    --model_name_or_path models/bge_large_en_ft_stage1 \
+    --input_file data/bge_dataset/train.jsonl \
+    --output_file data/bge_dataset/train_mined_hn_stage2.jsonl \
+    --range_for_sampling 1-25 \
+    --negative_number 15 \
+    --use_gpu_for_searching 
+
+torchrun --nproc_per_node 1 \
+    -m FlagEmbedding.baai_general_embedding.finetune.run \
+    --output_dir models/bge_large_en_ft_stage2 \
+    --model_name_or_path models/bge_large_en_ft_stage1 \
+    --train_data data/bge_dataset/train_mined_hn_stage2.jsonl \
+    --learning_rate 1e-5 \
+    --fp16 \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 16 \
+    --dataloader_drop_last True \
+    --normlized True \
+    --temperature 0.02 \
+    --query_max_len 150 \
+    --passage_max_len 35 \
+    --train_group_size 2 \
+    --negatives_cross_device \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --query_instruction_for_retrieval "" 
+
+# TODO add validation here
+
+# TODO try grad ckpt
