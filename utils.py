d6bbb1183c76aca64759cc93f6cbfd971f98583b
Evan Arlian <evanarlian2000@gmail.com>
2024-11-23 18:50:16 +0700

    remove old code

--- utils.py
+++ utils.py
@@ -1,20 +1,10 @@
-import numpy as np
+from datetime import datetime
+
 import pandas as pd
+import pytz
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-
-
-def merge_with_misconceptions(
-    df_train: pd.DataFrame, df_mis: pd.DataFrame
-) -> pd.DataFrame:
-    mis_dict = df_mis["MisconceptionName"].to_dict()
-    df_merged = df_train.copy()
-    for letter in "ABCD":
-        df_merged[f"Misconception{letter}Name"] = df_merged[
-            f"Misconception{letter}Id"
-        ].apply(lambda x: None if np.isnan(x) else mis_dict[int(x)])
-    return df_merged
 
 
 def make_nice_df(df: pd.DataFrame) -> pd.DataFrame:
@@ -181,3 +171,12 @@ def manual_late_interaction(queries: list[Tensor], docs: list[Tensor]) -> Tensor
             d = F.normalize(d, dim=-1)
             li[i, j] = (q @ d.T).max(-1).values.sum(-1)
     return li
+
+
+def wib_now() -> str:
+    """Generate current WIB time. Quite easy to read imho.
+    Example: 2024-11-23__17.30.02
+    """
+    wib = pytz.timezone("Asia/Jakarta")
+    timestamp = datetime.now(wib).strftime("%Y-%m-%d__%H.%M.%S")
+    return timestamp
