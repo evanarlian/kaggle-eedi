f9be914e161717f8d4062dcbd1db49fd77b39ca5
Evan Arlian <evanarlian2000@gmail.com>
2024-11-30 18:57:46 +0700

    send failure too

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 set -Eeuxo pipefail
 
-# export WANDB_DISABLED='true'
+notify_success() {
+    ./scripts/telegram.sh "training embedding done"
+}
 
+notify_failure() {
+    ./scripts/telegram.sh "training failed"
+}
+
+# Trap errors and call the failure notification
+trap notify_failure ERR
+
+# Main script logic
 torchrun --nproc-per-node=2 eedi/train_sts_hn.py \
     --paraphrased-path=data/eedi-paraphrased \
     --model=Alibaba-NLP/gte-large-en-v1.5 \
-    --per-device-bs=32 \
+    --per-device-bs=64 \
     --lr=1e-4 \
-    --n-epochs=5 \
+    --n-epochs=3 \
     --lora-rank=16 \
     --run-name=gte-large-proper
 
-./scripts/telegram.sh "training embedding done"
+# If training succeeds, send success notification
+notify_success
