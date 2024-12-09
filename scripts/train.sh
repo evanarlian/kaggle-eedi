63bf30a3c2be0b66ed9ebde1a3cf03c12e193e8f
Evan Arlian <evanarlian2000@gmail.com>
2024-12-09 23:05:41 +0700

    train 4 qwens

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuxo pipefail
+set -Euxo pipefail
 
 notify_failure() {
     ./scripts/telegram.sh "training failed"
@@ -14,60 +14,56 @@ accelerate launch eedi/train_sts_hn.py \
     --use-synthetic \
     --model=Qwen/Qwen2.5-1.5B-Instruct \
     --token-pool=last \
-    --per-device-bs=8 \
-    --lr=2e-4 \
-    --warmup-ratio=0.2 \
+    --per-device-bs=32 \
+    --lr=1e-4 \
+    --warmup-ratio=0.1 \
     --n-epochs=3 \
-    --lora-rank=16 \
+    --lora-rank=32 \
     --dataset-seed=1 \
     --iterative-hnm \
     --run-name=synthetic-qwen-instruct
 ./scripts/telegram.sh "synthetic-qwen-instruct done"
 
-# # 2
-# accelerate launch eedi/train_sts_hn.py \
-#     --synthetic-path=data/eedi-synthetic \
-#     --use-synthetic \
-#     --model=Qwen/Qwen2.5-1.5B-Instruct \
-#     --token-pool=last \
-#     --per-device-bs=8 \
-#     --lr=2e-4 \
-#     --warmup-ratio=0.2 \
-#     --n-epochs=5 \
-#     --lora-rank=16 \
-#     --dataset-seed=1 \
-#     --iterative-hnm \
-#     --run-name=synthetic-qwen
-# ./scripts/telegram.sh "synthetic-qwen done"
+# 2
+accelerate launch eedi/train_sts_hn.py \
+    --synthetic-path=data/eedi-synthetic \
+    --use-synthetic \
+    --model=Qwen/Qwen2.5-1.5B \
+    --token-pool=last \
+    --per-device-bs=8 \
+    --lr=2e-5 \
+    --warmup-ratio=0.2 \
+    --n-epochs=1 \
+    --lora-rank=16 \
+    --dataset-seed=2 \
+    --run-name=synthetic-qwen
+./scripts/telegram.sh "synthetic-qwen done"
 
-# # 3
-# accelerate launch eedi/train_sts_hn.py \
-#     --synthetic-path=data/eedi-synthetic \
-#     --use-synthetic \
-#     --model=Qwen/Qwen2.5-1.5B-Instruct \
-#     --token-pool=last \
-#     --per-device-bs=8 \
-#     --lr=2e-4 \
-#     --warmup-ratio=0.2 \
-#     --n-epochs=5 \
-#     --lora-rank=16 \
-#     --dataset-seed=1 \
-#     --iterative-hnm \
-#     --run-name=synthetic-qwen-math-instruct
-# ./scripts/telegram.sh "synthetic-qwen-math-instruct done"
+# 3
+accelerate launch eedi/train_sts_hn.py \
+    --synthetic-path=data/eedi-synthetic \
+    --model=Qwen/Qwen2.5-Math-1.5B-Instruct \
+    --token-pool=last \
+    --per-device-bs=24 \
+    --lr=7e-5 \
+    --warmup-ratio=0.3 \
+    --n-epochs=10 \
+    --lora-rank=64 \
+    --dataset-seed=3 \
+    --iterative-hnm \
+    --run-name=synthetic-qwen-math-instruct
+./scripts/telegram.sh "synthetic-qwen-math-instruct done"
 
-# # 4
-# accelerate launch eedi/train_sts_hn.py \
-#     --synthetic-path=data/eedi-synthetic \
-#     --use-synthetic \
-#     --model=Qwen/Qwen2.5-1.5B-Instruct \
-#     --token-pool=last \
-#     --per-device-bs=8 \
-#     --lr=2e-4 \
-#     --warmup-ratio=0.2 \
-#     --n-epochs=5 \
-#     --lora-rank=16 \
-#     --dataset-seed=1 \
-#     --iterative-hnm \
-#     --run-name=synthetic-qwen-math
-# ./scripts/telegram.sh "synthetic-qwen-math done"
+# 4
+accelerate launch eedi/train_sts_hn.py \
+    --synthetic-path=data/eedi-synthetic \
+    --model=Qwen/Qwen2.5-Math-1.5B \
+    --token-pool=last \
+    --per-device-bs=32 \
+    --lr=5e-5 \
+    --warmup-ratio=0.4 \
+    --n-epochs=8 \
+    --lora-rank=48 \
+    --dataset-seed=4 \
+    --run-name=synthetic-qwen-math
+./scripts/telegram.sh "synthetic-qwen-math done"
